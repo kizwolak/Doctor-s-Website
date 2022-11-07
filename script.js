f7279ba0d46cca6fb9f73f60d8b4d6d1ba7ba97d
Krzysztof <ki.zwolak@gmail.com>
2022-11-07 03:27:13 +0100

    Begin adding home page text

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let mobileMenu = document.querySelector(".mobileMenu");
 let mobileList = document.querySelector(".mobileList");
+let suwak = document.querySelector("#suwak");
 
 mobileMenu.addEventListener('click', () => {
     mobileList.classList.toggle('open');
+    suwak.classList.toggle('openImg');
 })
