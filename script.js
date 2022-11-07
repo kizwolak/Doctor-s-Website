188ce23b6a707746d093c7d5f1b5e8e5fa949bb9
Krzysztof <ki.zwolak@gmail.com>
2022-11-07 04:39:31 +0100

    About me/CSS work

--- script.js
+++ script.js
@@ -2,25 +2,7 @@ let mobileMenu = document.querySelector(".mobileMenu");
 let mobileList = document.querySelector(".mobileList");
 let suwak = document.querySelector("#suwak");
 
-(function () {
-    let mobileList = document.createElement("ul")
-    mobileList.className = "mobileList";
-    let li1 = document.createElement("li");
-    let li2 = document.createElement("li");
-    let li3 = document.createElement("li");
-    let li4 = document.createElement("li");
-    let li5 = document.createElement("li");
-
-    li1.textContent = "O MNIE";
-    li2.textContent = "PRZYKŁADOWE OPERACJE";
-    li3.textContent = "MIEJSCA PRZYJĘĆ";
-    li4.textContent = "GABINET PRYWATNY"
-    li5.textContent = "OPERACJE PRYWATNE"
-
-}());
-
 mobileMenu.addEventListener('click', () => {
     mobileList.classList.toggle('open');
-
     suwak.classList.toggle('openImg');
 })
